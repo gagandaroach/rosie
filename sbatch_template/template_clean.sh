4ba0d5b49531997714325943020f3dcbc122e4c3
gagany <gagandaroach@gmail.com>
2020-02-24 21:41:51 -0600

    rename folder

--- sbatch_template/template_clean.sh
+++ sbatch_template/template_clean.sh
@@ -14,16 +14,21 @@ MCW_RESEARCH=/data/mcw_research
 INPUT_DIR=${MCW_RESEARCH}/stylegan2
 OUTPUT_DIR=0.5x_cleaned
 
-echo $TRAINING_DATA
+SCRIPT_PATH=/srv/data/mcw_research/stylegan2/run_training.py
+SCRIPT_ARGS="--num-gpus=8 --data-dir=$DATA_DIR --config=config-f --dataset=$TRAINING_DATA --mirror-augment=true --gamma=100"
+
+echo "$TRAINING_DATA"
 
 # Path to container
 container="/data/containers/msoe-tensorflow.sif"
 
 # Command to run inside container
-command="python /srv/data/mcw_research/stylegan2/run_training.py --num-gpus=8 --data-dir=$DATA_DIR --config=config-f --dataset=$TRAINING_DATA --mirror-augment=true --gamma=100"
+command="python $SCRIPT_PATH $SCRIPT_ARGS"
+
+echo "${command}"
 
 # Execute singularity container on node.
 srun singularity exec --nv -B /data:/data ${container} /usr/local/bin/nvidia_entrypoint.sh ${command}
 
-echo done we did it heck ya 
+echo done we did it heck ya
 date
