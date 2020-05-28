4469e67e1dd50a91b6c9f5b18e941a1001eb14f9
Gagan Daroach <daroachgb@msoe.edu>
2020-05-28 11:00:56 -0500

    working stylegan 1 training with slurm and sbatch

--- sbatch/stylegan_train.sh
+++ sbatch/stylegan_train.sh
@@ -4,21 +4,24 @@
 #SBATCH --nodes=1
 #SBATCH --gpus=8
 #SBATCH --cpus-per-gpu=8
-##SBATCH --time=10-1:0
+##SBATCH --time=13-1:0
 # format: <days>-<hours>:<minutes>
 
-SCRIPT_NAME="Template Job"
+SCRIPT_NAME="StyleGAN Train Script - MSOE MCW Research"
 MCW_RESEARCH=/data/mcw_research
 
-NETWORK_DIR=$MCW_RESEARCH/stylegan2
+NETWORK_DIR=$MCW_RESEARCH/stylegan/src
 DATASET_DIR=$MCW_RESEARCH/tiles/tfrecords
+
 TRAINING_DATASET=0.5x_cleaned
 
-SCRIPT_PATH=$NETWORK_DIR/run_training.py
-SCRIPT_ARGS="--num-gpus=8 --data-dir=$DATASET_DIR --config=config-f --dataset=$TRAINING_DATASET --mirror-augment=true --gamma=100"
+SCRIPT_PATH=$NETWORK_DIR/train.py
+
+#SCRIPT_ARGS="--num-gpus=8 --data-dir=$DATASET_DIR --config=config-f --dataset=$TRAINING_DATASET --mirror-augment=true --gamma=100"
 
 CONTAINER="/data/containers/msoe-tensorflow.sif"
-COMMAND="python $SCRIPT_PATH $SCRIPT_ARGS"
+
+#COMMAND="python $SCRIPT_PATH $SCRIPT_ARGS"
 
 ## SCRIPT
 echo "starting sbatch script: ${SCRIPT_NAME}"
@@ -28,8 +31,10 @@ echo Training Dataset: "$TRAINING_DATASET"
 echo Using Container: "$CONTAINER"
 echo Executing Command: "${COMMAND}"
 
-srun singularity exec --nv -B /data:/data ${CONTAINER} /usr/local/bin/nvidia_entrypoint.sh "${COMMAND}"
+srun singularity exec --nv -B /data:/data ${CONTAINER} python "${SCRIPT_PATH}"
+#srun singularity exec --nv -B /data:/data ${CONTAINER} "${COMMAND}"
 
+echo " "
 echo "done we did it heck ya"
 date
 ## END SCRIPT
