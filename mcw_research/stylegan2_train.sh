39e4e1e9c56f09c4a29b1a1a7d12d3cc46f5901c
Gagan Daroach <daroachgb@msoe.edu>
2020-06-07 03:46:12 -0500

    add result dir, job name

--- mcw_research/stylegan2_train.sh
+++ mcw_research/stylegan2_train.sh
@@ -4,16 +4,17 @@
 #SBATCH --nodes=1
 #SBATCH --gpus=8
 #SBATCH --cpus-per-gpu=8
+#SBATCH --job-name=SG2_100
 
 SCRIPT_NAME="StyleGAN2 Train Script - MSOE MCW Research"
-
 WORKSPACE=/data/mcw_research
 SCRIPT_PATH=$WORKSPACE/stylegan2/run_training.py
+RESULT_DIR=$WORKSPACE/stylegan2/results
 DATASET_DIR=$WORKSPACE/tiles/tfrecords
 DATASET_NAME="0.5x_cleaned"
 GAMMA="100"
 
-SCRIPT_ARGS="--num-gpus=8 --data-dir=$DATASET_DIR --config=config-f --dataset=$DATASET_NAME --mirror-augment=false --gamma=$GAMMA"
+SCRIPT_ARGS="--num-gpus=8 --data-dir=$DATASET_DIR --result-dir=$RESULT_DIR --config=config-f --dataset=$DATASET_NAME --mirror-augment=false --gamma=$GAMMA"
 
 CONTAINER="/data/containers/msoe-tensorflow.sif"
 
