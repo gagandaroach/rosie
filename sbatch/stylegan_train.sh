11c46cbe5ee9ffbc3f137881cc90a74a6e39b265
gagany <gagandaroach@gmail.com>
2020-06-06 23:35:28 -0500

    clean up stylegan sbatch

--- sbatch/stylegan_train.sh
+++ sbatch/stylegan_train.sh
@@ -10,14 +10,7 @@
 SCRIPT_NAME="StyleGAN Train Script - MSOE MCW Research"
 MCW_RESEARCH=/data/mcw_research
 
-NETWORK_DIR=$MCW_RESEARCH/stylegan/src
-DATASET_DIR=$MCW_RESEARCH/tiles/tfrecords
-
-TRAINING_DATASET=0.5x_cleaned
-
-SCRIPT_PATH=$NETWORK_DIR/train.py
-
-#SCRIPT_ARGS="--num-gpus=8 --data-dir=$DATASET_DIR --config=config-f --dataset=$TRAINING_DATASET --mirror-augment=true --gamma=100"
+SCRIPT_PATH=$MCW_RESEARCH/stylegan/src/train.py
 
 CONTAINER="/data/containers/msoe-tensorflow.sif"
 
@@ -32,7 +25,6 @@ echo Using Container: "$CONTAINER"
 echo Executing Command: "${COMMAND}"
 
 srun singularity exec --nv -B /data:/data ${CONTAINER} python "${SCRIPT_PATH}"
-#srun singularity exec --nv -B /data:/data ${CONTAINER} "${COMMAND}"
 
 echo " "
 echo "done we did it heck ya"
